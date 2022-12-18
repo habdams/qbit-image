d4378dd2c958b2edc3834a9b44a2d2cffcc350d9
Adetunji Ojekunle <ojekunleadetunji@gmail.com>
2022-12-18 02:19:35 +0100

    Add <ImageGrid /> component to the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
+<script>
+    import ImageGrid from "$lib/Components/ImageGrid/ImageGrid.svelte";
+    import data from "$lib/data/image_data.json";
 
+    const listing = data.map(listing => listing.images)
+</script>
 
-<h1>This text needs to replaced with a component showing Airbnb style image grid</h1>
-<p>See <a href="https://www.airbnb.com/rooms/20238974">the link</a> for an example. The ideal component will make use of Flowbite-Svelte and Tailwind CSS</p>
-<p>The grid should be dynamic and interactive and work for multiple screen sizes similiar to the one on Airbnb</p>
-<p>Example images can be found in image_data.json</p>
-
+<section class="w-screen flex justify-center items-center flex-col">
+    <ImageGrid images={listing[0]} />
+</section>
